9a51f5d2670ffaf1c18aa1858f4ac4ba7f2a90a4
Cassandra <cassandra.donatelli@gmail.com>
2025-01-22 13:58:32 -0800

    fixed 3 pt bend for Julia

--- ExtractMTSDataBatch.R
+++ ExtractMTSDataBatch.R
@@ -8,19 +8,24 @@ names(files)[1] <- 'files'
 files$files<- as.character(files$files)
 finalResult <- data.frame()
 
-### Change this to equal the span of your 3pt bending rig
-# length = 50
+### Change this to equal the span of your 3pt bending rig (mm)
+length = 50
+
+### Change this to get values at a specific displacement and load
+setDisp = 15
+setLoad = 50
 
 for (i in 1:nrow(files)){
-  data <- read.csv(files$files[i])
-  result <- crush(data)
+  #data <- read.csv(files$files[i])
+  data <- read_csv(files$files[i], skip = 6)
+  data = data[-1,]
+  result <- bend_3pt(data, length, setDisp, setLoad)
   finalResult <- rbind(finalResult, result)
-  readline(prompt="Press [enter] to continue")
+  #readline(prompt="Press [enter] to continue")
 }
 
 row.names(finalResult) <- files$files
 #row.names(finalResult) <- files[2:5,]
-
 
 ### Change this to a file name that makes sense
 fileName = "testDemoThing.csv"
--- bend_3pt.R
+++ bend_3pt.R
@@ -1,34 +1,37 @@
 
-  bend_3pt <- function(data, length){
+  bend_3pt <- function(data, length, setDisp, setLoad){
     #pracma package required for trapz
+    
+    data$Time = as.numeric(data$Time)
+    data$Extension = as.numeric(data$Extension)
+    data$Load = as.numeric(data$Load)
     
     #data$Load <- abs(data$Load)
     data$h = sqrt((length/2)^2 + abs(data$Extension^2))
     data$Angle = rad2deg(asin(data$Extension * (sin(90)/data$h)))
     
     #Select break and change points
-    x11() # for some reason identify wants its own window now
-      plot(data$Time, data$Load, main = "Select the Change in Slope first. \n Select Hold Start second. \n Select Hold End third \n Then close window")      
-      userPts <- sapply(list(data$Time,data$Load),"[",identify(data$Time,data$Load, n=3))
-      
-      deltaSlopeIndex <- which(userPts[1] == data$Time)
-      ExtensionAtDeltaSlope = data$Extension[deltaSlopeIndex]
-      loadAtDeltaSlope = data$Load[deltaSlopeIndex]
-      angleAtDeltaSlope = data$Angle[deltaSlopeIndex]
-      
-      # breakIndex <- which(userPts[2] == data$Time)
-      # ExtensionAtBreak = data$Extension[breakIndex]
-      # loadAtBreak = data$Load[breakIndex]
-      # angleAtBreak = data$Angle[breakIndex]
-    
-    #Select hold time
-      plot(data$Time, data$Extension, main = "Select hold start, then hold stop, then click 'finish.'")
-      hold <- sapply(list(data$Time,data$Extension),"[",identify(data$Time,data$Extension))
-      holdStartIndex <- which(userPts[2] == data$Time)
-      loadAtHoldStart = data$Load[holdStartIndex]
-      
-      holdEndIndex <- which(userPts[3] == data$Time)
-      loadAtHoldEnd = data$Load[holdEndIndex]
+    #Comment out if you don't need break or hold data
+    #########
+      # x11() # for some reason identify wants its own window now
+      # plot(data$Time, data$Load, main = "Select the Change in Slope first. \n Select Hold Start second. \n Select Hold End third \n Then close window")      
+      # userPts <- sapply(list(data$Time,data$Load),"[",identify(data$Time,data$Load, n=3))
+      # 
+      # deltaSlopeIndex <- which(userPts[1] == data$Time)
+      # ExtensionAtDeltaSlope = data$Extension[deltaSlopeIndex]
+      # loadAtDeltaSlope = data$Load[deltaSlopeIndex]
+      # angleAtDeltaSlope = data$Angle[deltaSlopeIndex]
+      # 
+      # # breakIndex <- which(userPts[2] == data$Time)
+      # # ExtensionAtBreak = data$Extension[breakIndex]
+      # # loadAtBreak = data$Load[breakIndex]
+      # # angleAtBreak = data$Angle[breakIndex]
+      # 
+      # holdStartIndex <- which(userPts[2] == data$Time)
+      # loadAtHoldStart = data$Load[holdStartIndex]
+      # holdEndIndex <- which(userPts[3] == data$Time)
+      # loadAtHoldEnd = data$Load[holdEndIndex]
+    ##########
     
     #Find Max Values
       maxLoadIndex <- which.max(data$Load)
@@ -41,37 +44,53 @@
       loadAtMaxExt <- data$Load[maxExtensionIndex]
       angleAtMaxExt <- data$Angle[maxExtensionIndex]
     
-    #Find values at set displacement (5mm in this case)
+    
       # this is because we will pass 5 on the way up and down and we 
       # want values on the way up
-      dataLoading <- data[c(1:maxExtensionIndex),] 
-      setDispIndex <- which(abs(dataLoading$Extension - 5) == min(abs(dataLoading$Extension - 5)))
+      # dataLoading <- data[c(1:maxExtensionIndex),] # for cyclic
+      
+      #Find values at set displacement
+      setDispIndex <- which(abs(data$Extension - setDisp) == min(abs(data$Extension - setDisp)))
       loadAtSetDisp <- data$Load[setDispIndex]
       extensionAtSetDisp <- data$Extension[setDispIndex]
       angleAtSetDisp <- data$Angle[setDispIndex]
       
+    #Find values at set Load
+      setLoadIndex <- which(abs(data$Load - setLoad) == min(abs(data$Load - setLoad)))
+      dispAtSetLoad <- data$Extension[setLoadIndex]
+      LoadAtSetLoad <- data$Extension[setLoadIndex]
+      angleAtSetLoad <- data$Angle[setLoadIndex]
+      
     #Data Subsets
+      #### uncomment if you want break data
       # dataToBreak = data[c(1:breakIndex),]
+      # dataToDeltaSlope = data[c(1:deltaSlopeIndex),]
+      ####
+      
       dataToMaxLoad = data[c(1:maxLoadIndex),]
-      dataToDeltaSlope = data[c(1:deltaSlopeIndex),]
       dataToMaxExtension = data[c(1:maxExtensionIndex),]
       dataToSetDisp = data[c(1:setDispIndex),]
+      dataToSetLoad = data[c(1:setLoadIndex),]
     
     #Work Calculations
+      #### uncomment if you want break data
       # workToBreak <- trapz(dataToBreak$Extension, dataToBreak$Load)
-      workToDeltaSlope <- trapz(dataToDeltaSlope$Extension, dataToDeltaSlope$Load)
+      # workToDeltaSlope <- trapz(dataToDeltaSlope$Extension, dataToDeltaSlope$Load)
+      ####
       workToMaxLoad <- trapz(dataToMaxLoad$Extension, dataToMaxLoad$Load)
       workToMaxExtension <- trapz(dataToMaxExtension$Extension, dataToMaxExtension$Load)
       workToSetDisp <- trapz(dataToSetDisp$Extension, dataToSetDisp$Load)
+      workToSetLoad <- trapz(dataToSetLoad$Extension, dataToSetLoad$Load)
       totalWork <- trapz(data$Extension, data$Load)
     
       results <- data.frame(length, totalWork, 
                             maxLoad, angleAtMaxLoad, extensionAtMaxLoad, workToMaxLoad,
                             loadAtMaxExt, angleAtMaxExt, maxExtension, workToMaxExtension,
                             # loadAtBreak, angleAtBreak, ExtensionAtBreak, workToBreak,
-                            loadAtHoldStart, loadAtHoldEnd,
-                            loadAtDeltaSlope, angleAtDeltaSlope, ExtensionAtDeltaSlope, workToDeltaSlope,
-                            loadAtSetDisp, extensionAtSetDisp, angleAtSetDisp, workToSetDisp)
+                            # loadAtHoldStart, loadAtHoldEnd,
+                            # loadAtDeltaSlope, angleAtDeltaSlope, ExtensionAtDeltaSlope, workToDeltaSlope,
+                            loadAtSetDisp, extensionAtSetDisp, angleAtSetDisp, workToSetDisp,
+                            dispAtSetLoad, LoadAtSetLoad, angleAtSetLoad, workToSetLoad)
     return(results)
   }
   
